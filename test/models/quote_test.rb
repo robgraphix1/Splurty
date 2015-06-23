d2f5d7316ce445d8580d0d31f7757190a9320897
Robert Perez <ptelerez@cox.net>
2015-06-23 01:14:24 +0000

    added a unique_id method via TDD

--- test/models/quote_test.rb
+++ test/models/quote_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class QuoteTest < ActiveSupport::TestCase
-   test "the truth" do
-     assert true
-   end
+  test "unique_tag"  do
+  	quote = Quote.create(:author => 'Rob Perez', :saying => 'Yiya boyeee.')	
+  	expected = 'RP#' + quote.id.to_s
+  	actual = quote.unique_tag
+  	assert_equal expected, actual
+  end
+  	
 end
